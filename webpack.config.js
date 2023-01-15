8fbaed6dbaf329872878485374be8937c80cb125
ilonaand <ilonaand@gmail.com>
2023-01-15 21:51:31 +0300

    fix: prod mode

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/index.ts',
+  target: 'node',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.cjs',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
